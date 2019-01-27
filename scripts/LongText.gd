1de2e69a72584b290a469569e5be9e7e85c42096
Christian Oliveros <christianol_01@hotmail.com>
2019-01-26 22:51:04 -0400

    Long Text

--- scripts/LongText.gd
+++ scripts/LongText.gd
@@ -4,10 +4,13 @@ extends RichTextLabel
 # var a = 2
 export(float, 0, 20, 0.01) var lineWaitTime = 2
 export(float, 0, 1, 0.00001) var charWaitTime = 0.001
+export(float, 0, 1, 0.0000001) var vibrateWaitTime = 0.001
 export(String, MULTILINE) var longText
 export(bool) var start = false
 export(bool) var startOnFirstLine = false
+export(bool) var vibrate = false
 export(bool) var done = false
+export(float, 0, 20, 0.01) var vibrateMax = 20
 
 var arrText = []
 var currText = ""
@@ -16,6 +19,7 @@ var targetChar = 0
 
 var lineAccTime = 0
 var charAccTime = 0
+var vibrateAccTime = 0
 var charComplete = false
 var textSize = 0
 var textComplete = false
@@ -26,15 +30,20 @@ var imgRegex = RegEx.new()
 var bbcodeRegex = RegEx.new()
 var charWaitRegex = RegEx.new()
 var lineWaitRegex = RegEx.new()
+var vibrateWaitRegex = RegEx.new()
+
+var textCenter =  Vector2(0,0)
 
 var background = null
 
 func reset_text(state):
 	start = state
 	done = false
-	config_text()
 	if background == null:
 		background = get_parent()
+		textCenter = rect_position
+	config_text()
+	
 	
 	if start:
 		background.show()
@@ -48,6 +57,7 @@ func _ready():
 	
 
 func config_text():
+	rect_position = textCenter
 	if longText != null:
 		arrText = longText.split("\n")
 		textSize = arrText.size()
@@ -75,6 +85,7 @@ func config_text():
 	bbcodeRegex.compile("\\[[^\\]]*\\]")
 	charWaitRegex.compile("\\[char=(?<val>[-+]?[0-9]*\\.?[0-9]+)\\]")
 	lineWaitRegex.compile("\\[line=(?<val>[-+]?[0-9]*\\.?[0-9]+)\\]")
+	vibrateWaitRegex.compile("\\[vibrate=(?<val>[-+]?[0-9]*\\.?[0-9]+)(,(?<val2>[-+]?[0-9]*\\.?[0-9]+))?\\]")
 	
 
 
@@ -92,6 +103,7 @@ func animateText(delta):
 				lineAccTime += delta
 			else:
 				print(lineAccTime)
+
 				currLine = arrText[curPos].replace("\\n", "\n")
 				currLine = currLine.replace("\\t", "\t")
 
@@ -99,6 +111,9 @@ func animateText(delta):
 					currText = ""
 					currLine = currLine.replace("\\c", "")
 					print("Clear")
+
+				vibrate = false
+				rect_position = textCenter
 				
 				var regexRes = charWaitRegex.search(currLine)
 
@@ -114,6 +129,25 @@ func animateText(delta):
 					lineWaitTime =  float(regexRes.get_string("val"))
 					print("lineWait=" + regexRes.get_string("val"))
 
+				regexRes = vibrateWaitRegex.search(currLine)
+
+				if regexRes:
+					currLine = vibrateWaitRegex.sub(currLine, "")
+					vibrateWaitTime = float(regexRes.get_string("val"))
+					vibrate = true
+					var val2 = regexRes.get_string("val2")
+					if val2:
+						vibrateMax = float(val2)
+						print("vibrate=" + regexRes.get_string("val") + ", " + val2)
+					else:
+						print("vibrate=" + regexRes.get_string("val"))
+
+				if currLine.find("[vibrate]") != -1:
+					currText = ""
+					currLine = currLine.replace("[vibrate]", "")
+					vibrate = true
+					print("vibrate")
+
 				currLine += "\n"
 				
 				visible_characters = 0
@@ -125,6 +159,8 @@ func animateText(delta):
 				charAccTime = 0
 				linePos = 0
 				charComplete = false
+				
+				vibrateAccTime = vibrateWaitTime
 				textComplete = curPos >= textSize
 				print("Line Complete")
 		else:
@@ -139,12 +175,18 @@ func animateText(delta):
 				targetChar -= advance
 				linePos += advance
 				
-				
 				if targetChar <= 0:
 					charComplete = true
 					charAccTime = 0
 					lineAccTime = 0
 					print("Char Complete")
+
+			if vibrate:
+				if vibrateAccTime < vibrateWaitTime:
+					vibrateAccTime += delta
+				else:
+					vibrateAccTime = 0
+					vibrate()
 	else:
 		if lineAccTime < lineWaitTime:
 			lineAccTime += delta
@@ -171,3 +213,7 @@ func charsToSkip():
 	
 	
 
+func vibrate():
+	var mov = (randf() * 2.0 - 1.0) * vibrateMax 
+
+	rect_position = textCenter + Vector2(0, mov)
