c587078e2f7d653209ba700b6e8ff4cf36f3d7ea
callmeanto <antonellalrc@gmail.com>
2019-01-27 11:14:25 -0400

    agrega mensaje cuando el usuario toca silla en escena de bus

--- SceneManager.gd
+++ SceneManager.gd
@@ -15,3 +15,19 @@ func _on_try_to_leave(body):
 			if change_song:
 				AudioManager.changeSong(next_song)
 			get_tree().change_scene(next_scene)
+
+func hola(body):
+	if not (body is StaticBody2D):
+		get_node(poem_container_path).reset_text(true)
+
+
+func salir(body):
+	if not (body is StaticBody2D):
+		if get_node(poem_container_path).done:
+			get_node(poem_container_path).longText = "time to go back and shine like a star"
+			get_node(poem_container_path).reset_config()
+			get_node(poem_container_path).reset_text(true)
+			yield(get_tree().create_timer(6), "timeout")
+			if change_song:
+				AudioManager.changeSong(next_song)
+			get_tree().change_scene(next_scene)
